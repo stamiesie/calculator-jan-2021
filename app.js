8b6f68fabd7fd02ebba1e1c846206b18de0b43f0
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-01-07 12:03:23 -0800

    add functionality

--- app.js
+++ app.js
@@ -1,36 +1,73 @@
-// import functions and grab DOM elements
+const additionField = document.getElementById('add-input-1');
 
-// initialize state
+const additionField2 = document.getElementById('add-input-2');
 
-// set event listeners to update state and DOM
+const addButton = document.getElementById('add');
 
-const welcomeElement = document.getElementById('welcome');
+const addResults = document.getElementById('add-results');
 
-console.log(welcomeElement);
+console.log(additionField);
+console.log(additionField2);
+console.log(addResults);
+console.log(addButton);
 
-const myButton = document.getElementById('change');
-const input = document.getElementById("input");
-inputElement = document.getElementById("user-input");
+addButton.addEventListener('click', () => {
+    const fieldValue = Number(additionField.value);
+    const fieldValue2 = Number(additionField2.value);
 
-myButton.addEventListener('click', () => {
-    welcomeElement.style.color = "red";
-    welcomeElement.textContent = input.value;
+    const result = fieldValue + fieldValue2;
+
+    addResults.textContent = result;
+
 });
 
-console.log("It Worked!!!!");
+const subtractionField = document.getElementById('subtract-input-1');
 
+const subtractionField2 = document.getElementById('subtract-input-2');
 
-const paragraph = document.getElementById("words");
+const subtractButton = document.getElementById('subtract');
 
-button2.addEventListener('click', () => {
-    paragraph.style.color = "red";
+const subtractResults = document.getElementById('subtract-results');
+
+subtractButton.addEventListener('click', () => {
+    const subValue = subtractionField.valueAsNumber;
+    const subValue2 = subtractionField2.valueAsNumber;
+
+    const diff = subValue - subValue2;
+
+    subtractResults.textContent = diff;
 });
 
-const background = document.getElementById("page");
+const multField = document.getElementById('mult-input-1');
 
-console.log("Change the background!");
+const multField2 = document.getElementById('mult-input-2');
 
-button3.addEventListener('click', () => {
-    page.style.background = "black";
+const multButton = document.getElementById('mult');
+
+const multResults = document.getElementById('mult-results');
+
+multButton.addEventListener('click', () => {
+    const multValue = multField.valueAsNumber;
+    const multValue2 = multField2.valueAsNumber;
+
+    const product = multValue * multValue2;
+
+    multResults.textContent = product;
 });
+
+
+const divField = document.getElementById('divide-input-1');
+
+const divField2 = document.getElementById('divide-input-2');
+
+const divButton = document.getElementById('divide');
+
+const divResults = document.getElementById('divide-results');
+
+divButton.addEventListener('click', () => {
+    const quot = divField.valueAsNumber / divField2.valueAsNumber;
+
+    divResults.textContent = quot;
+});
+
 
